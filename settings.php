3455c7fd300e372b129c1c3e2c8bcdabd64f2f2b
Daniel Meißner <daniel.meissner@uni-muenster.de>
2025-12-05 14:41:10 +0100

    Use admin settings for EvaSys connection settings

--- settings.php
+++ settings.php
@@ -17,9 +17,53 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-if ($hassiteconfig) { // Needs this condition or there is error on login page.
+if ($hassiteconfig) {
     $ADMIN->add('blocksettings', new admin_externalpage('block_evasys_sync',
         get_string('evasys_sync', 'block_evasys_sync'),
         new moodle_url('/blocks/evasys_sync/adminsettings.php')));
+
+    $ADMIN->add('blocksettings', new admin_category(
+        'block_evasys_sync_settings',
+        new lang_string('evasys_sync', 'block_evasys_sync')));
+    $settingspage = new admin_settingpage(
+        'manageevasyssync', new lang_string('evasys_sync', 'block_evasys_sync'));
+
+    if ($ADMIN->fulltree) {
+        $settingspage->add(new admin_setting_heading(
+            'block_evasys_connection_settings',
+            'Connection settings', 'These are the necessary settings to connect to the external EvaSys instance.'));
+        $settingspage->add(new admin_setting_configtext(
+            'block_evasys_sync/evasys_username',
+            get_string('settings_username', 'block_evasys_sync'),
+            '',
+            '',
+            PARAM_TEXT
+        ));
+
+        $settingspage->add(new admin_setting_configpasswordunmask(
+            'block_evasys_sync/evasys_password',
+            get_string('settings_password', 'block_evasys_sync'),
+            '',
+            ''
+        ));
+
+        $settingspage->add(new admin_setting_configtext(
+            'block_evasys_sync/evasys_soap_url',
+            get_string('settings_soap_url', 'block_evasys_sync'),
+            '',
+            '',
+            PARAM_TEXT
+        ));
+
+        $settingspage->add(new admin_setting_configtext(
+            'block_evasys_sync/evasys_wsdl_url',
+            get_string('settings_wsdl_url', 'block_evasys_sync'),
+            '',
+            '',
+            PARAM_TEXT
+        ));
+    }
+
+    $ADMIN->add('blocksettings', $settingspage);
 }
 $settings = null;
